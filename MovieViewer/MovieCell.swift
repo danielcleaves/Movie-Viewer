5bebcc29f8f78334c53d2bc28cc34b7a59d00860
daniel <daniel_cleaves@hotmail.com>
2016-10-17 22:02:56 -0500

    Added XCode Files

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Daniel Cleaves on 10/15/16.
+//  Copyright © 2016 Daniel Cleaves. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
